a9f21ae5071252838de8a65817fbebc4911811d4
F180243 <f180243@nu.edu.pk>
2022-10-26 12:45:23 +0500

    Input text field design completed

--- app/src/main/java/com/example/jettipapp/MainActivity.kt
+++ app/src/main/java/com/example/jettipapp/MainActivity.kt
@@ -1,6 +1,7 @@
 package com.example.jettipapp
 
 import android.os.Bundle
+import android.util.Log
 import androidx.activity.ComponentActivity
 import androidx.activity.compose.setContent
 import androidx.compose.foundation.BorderStroke
@@ -8,27 +9,36 @@ import androidx.compose.foundation.layout.*
 import androidx.compose.foundation.shape.CircleShape
 import androidx.compose.foundation.shape.CornerSize
 import androidx.compose.foundation.shape.RoundedCornerShape
+import androidx.compose.foundation.text.KeyboardActions
 import androidx.compose.material.MaterialTheme
 import androidx.compose.material.Surface
 import androidx.compose.material.Text
 import androidx.compose.runtime.Composable
+import androidx.compose.runtime.mutableStateOf
+import androidx.compose.runtime.remember
 import androidx.compose.ui.Alignment
+import androidx.compose.ui.ExperimentalComposeUiApi
 import androidx.compose.ui.Modifier
 import androidx.compose.ui.draw.clip
 import androidx.compose.ui.graphics.Color
+import androidx.compose.ui.platform.LocalSoftwareKeyboardController
 import androidx.compose.ui.text.font.FontWeight
 import androidx.compose.ui.tooling.preview.Preview
 import androidx.compose.ui.unit.dp
+import com.example.jettipapp.components.InputField
 import com.example.jettipapp.ui.theme.JetTipAppTheme
 import org.intellij.lang.annotations.JdkConstants
 
+
+@ExperimentalComposeUiApi
 class MainActivity : ComponentActivity() {
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         setContent {
 
                 App {
-                   TopHeader(23.9)
+                  // TopHeader(23.9)
+                    MainContext()
                 }
             }
         }
@@ -51,29 +61,17 @@ fun App(context: @Composable () -> Unit)
 }
 
 
+@ExperimentalComposeUiApi
 @Preview
 @Composable
 fun MainContext()
 {
-    Surface(modifier = Modifier
-        .padding(8.dp)
-        .fillMaxWidth(),
-    shape = RoundedCornerShape(corner = CornerSize(8.dp)),
-    border = BorderStroke(width = 1.dp, color = Color.LightGray)
-    )
-    {
-        Column() {
+    billForm( ){ billAmt ->
 
-        }
-
+        Log.d("AMT", "MainContext: $billAmt")
     }
-}
 
-@Composable
-fun Greeting(name: String) {
-    Text(text = "Hello $name!")
 }
-
 
 @Preview
 @Composable
@@ -107,6 +105,50 @@ fun TopHeader(totalPerPerson :Double = 0.0)
 @Composable
 fun DefaultPreview() {
     JetTipAppTheme {
-        Greeting("Android")
+
     }
+}
+
+@ExperimentalComposeUiApi
+@Composable
+fun billForm(modifier: Modifier = Modifier
+             ,onValChange: (String) -> Unit = {}
+)
+{
+    val totalBillState = remember {
+        mutableStateOf("")
+    }
+
+    val validState = remember (totalBillState.value) {
+
+        totalBillState.value.trim().isNotEmpty()
+    }
+    val keyboardController = LocalSoftwareKeyboardController.current
+
+    Surface(modifier = Modifier
+        .padding(8.dp)
+        .fillMaxWidth(),
+        shape = RoundedCornerShape(corner = CornerSize(8.dp)),
+        border = BorderStroke(width = 1.dp, color = Color.LightGray)
+    )
+    {
+        Column() {
+
+            InputField(
+                valueState = totalBillState ,
+                labelId = "Enter Bill",
+                enabled = true ,
+                isSingleLine = true,
+                onAction = KeyboardActions{
+
+                    if(!validState) return@KeyboardActions
+                     onValChange(  totalBillState.value.trim())
+                    keyboardController?.hide()
+                }
+            )
+        }
+
+    }
+
+
 }
